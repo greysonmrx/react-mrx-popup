5eaabd0a29f78685fcd62295455b828aa4c3c17c
unknown <greysonmrx@gmail.com>
2020-04-15 15:34:23 -0300

    feat: add outlined Button story

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -3,4 +3,6 @@ import { storiesOf } from "@storybook/react";
 
 import { Button } from "../src";
 
-storiesOf("Button", module).add("Default", () => <Button>Default</Button>);
+storiesOf("Button", module)
+  .add("Default", () => <Button>Default</Button>)
+  .add("Outlined", () => <Button outlined>Outlined</Button>);
