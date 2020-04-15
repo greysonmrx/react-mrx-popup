0aaf7b073ffa2ecd1787f8c92181605a8b38493f
unknown <greysonmrx@gmail.com>
2020-04-15 15:48:36 -0300

    feat: add button history with icon

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { MdSettings } from "react-icons/md";
 
 import { Button } from "../src";
 
 storiesOf("Button", module)
   .add("Default", () => <Button>Default</Button>)
   .add("Outlined", () => <Button outlined>Outlined</Button>)
-  .add("Disabled", () => <Button disabled>Disabled</Button>);
+  .add("Disabled", () => <Button disabled>Disabled</Button>)
+  .add("With Icon", () => <Button icon={<MdSettings />}>Settings</Button>);
