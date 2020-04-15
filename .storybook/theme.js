631b1c0c2d72c3e2af4073beac81b9c1dc48aaeb
unknown <greysonmrx@gmail.com>
2020-04-15 15:15:31 -0300

    feat: storybook typescript config

---
+++ .storybook/theme.js
@@ -0,0 +1,23 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+
+  colorSecondary: "#3333cc",
+
+  appBg: "#21222C",
+  appContentBg: "#282A36",
+  appBorderColor: "#3333cc",
+
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  textColor: "#FFFFFF",
+
+  barTextColor: "#FFFFFF",
+  barSelectedColor: "#212121",
+  barBg: "#1b1a1f",
+
+  brandTitle: "MRX UI",
+  brandUrl: "https://greysonmrx.github.io",
+});
