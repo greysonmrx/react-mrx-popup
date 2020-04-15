59c1d948ffdf3e96747a00bffbd80e4733b9fbb0
unknown <greysonmrx@gmail.com>
2020-04-15 16:05:25 -0300

    feat: add Input story with left icon

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
 import { storiesOf } from "@storybook/react";
+import { MdSearch } from "react-icons/md";
 
 import { Input } from "../src";
 
-storiesOf("Input", module).add("Default", () => {
-  const [inputValue, setInputValue] = useState("");
+storiesOf("Input", module)
+  .add("Default", () => {
+    const [inputValue, setInputValue] = useState("");
 
-  return (
-    <Input
-      name="first_name"
-      placeholder="First name"
-      value={inputValue}
-      setValue={setInputValue}
-    />
-  );
-});
+    return (
+      <Input
+        name="first_name"
+        placeholder="First name"
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  })
+  .add("With left icon", () => {
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+      <Input
+        name="search"
+        leftIcon={<MdSearch />}
+        placeholder="Search..."
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  });
